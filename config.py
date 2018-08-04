a77a3369412f74fb0aee0b1af0c6a0344ca5e0e3
Marwan Ali <marwanali2428@gmail.com>
2018-08-04 18:55:07 -0400

    Set up app database

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mircoblog-secret'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- mircroblog.py
+++ mircroblog.py
@@ -1,2 +1,9 @@
 # Python script at the top-level that defines the Flask application instance
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    shell_instances = {'db': db, 'User': User, 'Post': Post}
+    return shell_instances
