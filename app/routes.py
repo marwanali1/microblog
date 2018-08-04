daa8ed75895908aec84fe0dbb124f4ca7bebe46b
Marwan Ali <marwanali2428@gmail.com>
2018-08-04 02:48:50 -0400

    Removed license.

--- app/routes.py
+++ app/routes.py
@@ -27,7 +27,7 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remeber me={}'.format(
+        flash('Login requested for user {}, remember me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
