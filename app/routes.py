d613665dfbf8436a03f9d5adf19c30ca7b07cfda
Marwan Ali <marwanali2428@gmail.com>
2018-08-04 02:43:44 -0400

    Added a login screen

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from forms import LoginForm
 
 
 @app.route('/')
@@ -8,14 +10,24 @@ def index():
     View function with route URL decorators
     """
     user = {'username': 'Marwan'}
-    html_str = '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>
-    '''
-    return html_str
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remeber me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
