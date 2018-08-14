bf4a0b3c29258c42e9cbdee69d7e71881f55480e
Marwan Ali <marwanali2428@gmail.com>
2018-08-14 00:23:13 -0400

    Added user login and registration

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,8 @@
-from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user, logout_user, login_required
-
 from app import app, db
 from app.models import User
 from app.forms import LoginForm, RegistrationForm
-
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 
